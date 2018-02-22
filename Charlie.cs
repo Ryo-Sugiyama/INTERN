68b3fc4124013cd427c1e11cea581f3ce463abcf
youtsuumochii <sugisugirr@gmail.com>
2018-02-22 12:05:42 +0900

    First Commit

---
+++ Charlie.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Ateam
+{
+    public class Charlie : BaseBattleAISystem
+    {
+        //---------------------------------------------------
+        // InitializeAI
+        //---------------------------------------------------
+        override public void InitializeAI()
+        {
+        }
+
+        //---------------------------------------------------
+        // UpdateAI
+        //---------------------------------------------------
+        override public void UpdateAI()
+        {
+        }
+
+        //---------------------------------------------------
+        // ItemSpawnCallback
+        //---------------------------------------------------
+        override public void ItemSpawnCallback(ItemSpawnData itemData)
+        {
+        }
+    }
+}
